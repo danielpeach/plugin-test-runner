278f504cce6ab9206601ee3a4b14b94fa9b6ea4c
Dan Peach <daniel.peach@armory.io>
2020-10-29 10:49:37 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -96,12 +96,11 @@ const exec_1 = __webpack_require__(514);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const versions = JSON.stringify(JSON.parse(core.getInput('versions'))).replace(/\\([\s\S])|(")/g, '\\$1$2');
             const initGradle = `
 import groovy.json.JsonSlurper
 
 allprojects { project ->
-  def versions = new JsonSlurper().parseText("${versions}")
+  def versions = new JsonSlurper().parseText("""${core.getInput('versions')}""")
   project.afterEvaluate {
     def spinnakerPlugin = project.extensions.findByName("spinnakerPlugin")
     if (spinnakerPlugin != null) {
@@ -118,7 +117,7 @@ allprojects { project ->
             fs.writeFileSync('init.gradle', initGradle);
             const command = `./gradlew -I init.gradle test`;
             core.info(`Running command: ${command}`);
-            exec_1.exec(command);
+            yield exec_1.exec(command);
         }
         catch (error) {
             core.setFailed(error.message);
