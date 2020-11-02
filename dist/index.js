df2975f80e9e9607873426dd5022c11b82f5d151
danielpeach <danielpeach@users.noreply.github.com>
2020-11-02 16:55:48 +0000

    commit build to dist/

--- dist/index.js
+++ dist/index.js
@@ -722,7 +722,7 @@ const exec_1 = __webpack_require__(514);
 const artifact_1 = __webpack_require__(605);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        const payload = core.getInput('versions');
+        const payload = core.getInput('payload');
         try {
             const initGradle = `
 import groovy.json.JsonSlurper
@@ -731,7 +731,7 @@ allprojects { project ->
   def versions = new JsonSlurper().parseText("""${payload}""")
   project.afterEvaluate {
     def spinnakerPlugin = project.extensions.findByName("spinnakerPlugin")
-    if (spinnakerPlugin != null) {
+    if (spinnakerPlugin != null && versions[spinnakerPlugin.serviceName] != null) {
       def service = spinnakerPlugin.serviceName
       def serviceVersion = versions[spinnakerPlugin.serviceName]
       def platform = project.dependencies.platform("com.netflix.spinnaker.\${service}:\${service}-bom:$serviceVersion") {
@@ -750,11 +750,11 @@ allprojects { project ->
         catch (error) {
             core.setFailed(error.message);
         }
-        if (process.env['CI']) {
+        const runID = process.env['GITHUB_RUN_ID'];
+        const encodedPayload = Buffer.from(payload).toString('base64');
+        const artifactName = `${runID}-${encodedPayload}`;
+        if (core.getInput('skip_upload') !== 'true') {
             try {
-                const runID = process.env['GITHUB_RUN_ID'];
-                const encodedPayload = Buffer.from(payload).toString('base64');
-                const artifactName = `${runID}-${encodedPayload}`;
                 core.info(`Uploading dummy artifact ${artifactName}`);
                 const artifactClient = artifact_1.create();
                 const response = yield artifactClient.uploadArtifact(artifactName, ['init.gradle'], '.', {});
@@ -765,6 +765,11 @@ allprojects { project ->
             catch (error) {
                 core.setFailed(error.message);
             }
+        }
+        else {
+            core.info('Not in a CI environment');
+            core.info(`Raw payload is ${payload}`);
+            core.info(`Would have uploaded a artifact with the name '${artifactName}'`);
         }
     });
 }
